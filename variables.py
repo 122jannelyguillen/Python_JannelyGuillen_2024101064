124e9a88e794828891ddea1827a9207eba71e93c
122jannelyguillen <mguillen04@icloud.com>
2025-04-25 14:51:10 -0400

    Add files via upload
    Clase 2

---
+++ variables.py
@@ -0,0 +1,25 @@
+print ("Hola Mundo")
+
+entero= 50
+float= 50.1
+
+print (entero)
+palabra= "Hola de nuevo"
+dato=True
+print (type (entero))
+print(float)
+print (type (float))
+print (palabra)
+print(type (palabra))
+print (dato)
+print(type(dato))
+print ("'Sumando")
+num1=1
+print ("La suma es", numl+num2)
+print ("Cambio de variable")
+dato=2
+print (dato)
+print(type(dato))
+dato="Otro valor"
+print (dato)
+print(type(dato))
