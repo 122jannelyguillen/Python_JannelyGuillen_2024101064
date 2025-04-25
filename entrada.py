33d0c0f493ca3435f84459abf05028de52d4c3ba
122jannelyguillen <mguillen04@icloud.com>
2025-04-25 15:25:30 -0400

    entrada.py

---
+++ entrada.py
@@ -0,0 +1,20 @@
+```
+Entradas de datos
+```
+cadena = input("¿Como se llama tu proyecto?")
+print(f"Tu proyecto se llama: {cadena)")
+
+#cadena = input("¿Que version es")
+
+#print(f"Tu proyecto se llama: {cadena+1}")
+
+cadena = int (input ("¿Que version es?"))
+print(f"Tu proyecto se llama: {cadena+1}")
+
+
+```
+Salida de datos
+```
+app="python"
+proyecto="com.unida"
+print(f"Se hara {app} se limara {proyecto)")
